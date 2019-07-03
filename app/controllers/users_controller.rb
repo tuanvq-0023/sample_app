d09809d31aa40e66a910c9ad04e368b483d43bc3
Vo Quoc Tuan <vo.quoc.tuan@sun-asterisk.com>
2019-07-03 12:30:32 +0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
+      log_in @user
       flash[:success] = t ".welcome_to_the_sample_app"
       redirect_to @user
     else
