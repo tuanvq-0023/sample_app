1aff219875f94d390769d0d6b449398469af16c0
Vo Quoc Tuan <vo.quoc.tuan@sun-asterisk.com>
2019-07-04 09:21:56 +0700

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,12 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
+    user = get_user
+    rmb_set = Settings.remember_checked_default_value
 
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == rmb_set ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = t ".invalid_user_info"
@@ -14,7 +16,13 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def get_user
+    User.find_by email: params[:session][:email].downcase
   end
 end
